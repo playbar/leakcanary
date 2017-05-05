36b4a193fca0223dbe0eaa7788a5421fc0d2cfae
playbar <hgl868@126.com>
2017-05-05 14:53:08 +0800

    添加代码

--- leakcanary-sample/src/main/java/com/example/leakcanary/ExampleApplication.java
+++ leakcanary-sample/src/main/java/com/example/leakcanary/ExampleApplication.java
@@ -16,12 +16,22 @@
 package com.example.leakcanary;
 
 import android.app.Application;
+import android.content.Context;
 import android.os.StrictMode;
 import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
 
 public class ExampleApplication extends Application {
 
-  @Override public void onCreate() {
+  public static RefWatcher getRefWatcher(Context context) {
+    ExampleApplication application = (ExampleApplication) context.getApplicationContext();
+    return application.refWatcher;
+  }
+
+  private RefWatcher refWatcher;
+
+  @Override
+  public void onCreate() {
     super.onCreate();
     if (LeakCanary.isInAnalyzerProcess(this)) {
       // This process is dedicated to LeakCanary for heap analysis.
--- leakcanary-sample/src/main/java/com/example/leakcanary/MainActivity.java
+++ leakcanary-sample/src/main/java/com/example/leakcanary/MainActivity.java
@@ -21,15 +21,22 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.View;
 
+import com.squareup.leakcanary.RefWatcher;
+
 public class MainActivity extends Activity {
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main_activity);
 
+    RefWatcher refWatcher = ExampleApplication.getRefWatcher(this);
+    refWatcher.watch(this);
+
     View button = findViewById(R.id.async_task);
     button.setOnClickListener(new View.OnClickListener() {
-      @Override public void onClick(View v) {
+      @Override
+      public void onClick(View v) {
         startAsyncTask();
       }
     });
@@ -40,7 +47,8 @@ public class MainActivity extends Activity {
     // class MainActivity. If the activity gets destroyed before the task finishes (e.g. rotation),
     // the activity instance will leak.
     new AsyncTask<Void, Void, Void>() {
-      @Override protected Void doInBackground(Void... params) {
+      @Override
+      protected Void doInBackground(Void... params) {
         // Do some slow work in background
         SystemClock.sleep(20000);
         return null;
